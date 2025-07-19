ae2a297741cf2742b86ecb62ff9b8871aa8c5937
CesarMartinez <cesarwamartinez@gmail.com>
2025-07-19 18:59:13 -0500

    Link externos formateados

--- src/ui/formatDataLabel.tsx
+++ src/ui/formatDataLabel.tsx
@@ -17,11 +17,11 @@ const FormatDataTypeLabel = ({ data }: { data: JsonValue }) => {
   if (typeof data === "string" && collapsedLabel && data.length >= 30) {
     return (
       <span
-        className="text-cyan-400 hover:text-emerald-300 hover:text-shadow-xs transition-all duration-500"
-        title="Abreme que soy la tengo muy larga 😈"
+        className="text-pink-400 hover:text-emerald-300 hover:text-shadow-xs transition-all duration-500"
+        title="Abreme porque la tengo muy larga 😈"
         onClick={() => setCollapsedLabel(false)}
       >
-        &quot;{String(data.slice(0, 50).concat("..."))}&quot;{""}
+        &quot;{String(data.slice(0, 50).concat("..."))}&quot;
         &quot;<b className="text-[9px] p-1 rounded-md text-zinc-200 bg-gradient-to-t from-zinc-900 to-zinc-700">
           {typeof data}
         </b>{" "}
@@ -29,16 +29,35 @@ const FormatDataTypeLabel = ({ data }: { data: JsonValue }) => {
     );
   }
 
-  if (typeof data === "string" && collapsedLabel && data.length < 30) {
+
+    if (typeof data === "string" && data.startsWith("http")) {
+    return (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={data}
+        className="text-lime-400  hover:text-shadow-xs transition-all underline duration-500"
+        title="Link externo"
+      >
+        &quot;{String(data)}&quot;{""}
+        <b className="text-[9px] underline-none p-1 rounded-md text-zinc-200 bg-gradient-to-t from-zinc-900 to-zinc-700">
+          {typeof data}
+        </b>{" "}
+      </a>
+    );
+  }
+  
+
+  if (typeof data === "string" && collapsedLabel && data.length < 40) {
     return (
       <span
         className="text-green-400 hover:text-emerald-300 hover:text-shadow-xs transition-all duration-500"
         title="Click para expandir"
       >
-        &quot;{String(data)}&quot;{""}
+        &quot;{String(data)}&quot;
         <b className="text-[9px] p-1 rounded-md text-zinc-200 bg-gradient-to-t from-zinc-900 to-zinc-700">
           {typeof data}
-        </b>{" "}
+        </b>
       </span>
     );
   }
