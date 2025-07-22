9b0952f0a9969532eb012980c63182d1feed67b2
CesarMartinez <cesarwamartinez@gmail.com>
2025-07-22 18:18:03 -0500

    Add response

--- src/ui/Console.tsx
+++ src/ui/Console.tsx
@@ -1,12 +1,17 @@
-import React, { useState, useRef, useEffect } from "react";
-import JsonDiffViewerModal from "./DiffJson";
+import React, { useState, useRef } from "react";
 import { JsonNode } from "./Formatter";
 import toast from "react-hot-toast";
 
 export default function Console() {
-  const [consoleText, setConsoleText] = useState("https://jsonplaceholder.typicode.com/posts POST");
+  const [consoleText, setConsoleText] = useState(
+    "https://jsonplaceholder.typicode.com/posts POST",
+  );
   const [history, setHistory] = useState<
-    { command: string; output: object | string }[]
+    {
+      command: string;
+      output: object | string;
+      dataResponse: Response | null | undefined;
+    }[]
   >([]);
   const consoleRef = useRef<HTMLDivElement>(null);
 
@@ -14,19 +19,26 @@ export default function Console() {
     e.preventDefault();
 
     if (!consoleText.trim()) {
-      toast.error("Por favor introduzca texto")
+      toast.error("Por favor introduzca texto");
       return;
     }
 
-    const [endpoint, method = "GET"] = consoleText.split(" ");
+    if (consoleText.split(" ").length > 3) {
+      toast.error("Tal vez este mandando mas parametros de lo permitidos.");
+    }
+
+    const [endpoint, method = "GET", body = ""] = consoleText.split(" ");
 
     try {
-      const response = await fetch(endpoint, { method });
+      const response = await fetch(endpoint, { method, body: body.trim() });
+      const dataResponse = response;
       const data = await response.json();
+
+      console.log(dataResponse);
 
       setHistory((prev) => [
         ...prev,
-        { command: consoleText, output: data },
+        { command: consoleText, output: data, dataResponse },
       ]);
     } catch (error) {
       setHistory((prev) => [
@@ -38,44 +50,47 @@ export default function Console() {
     setConsoleText("");
   };
 
-//   useEffect(() => {
-//     // consoleRef.current?.scrollTo({
-//     //   top: consoleRef.current.scrollHeight,
-//     //   behavior: "smooth",
-//     // });
-//   }, [history]);
+  //   useEffect(() => {
+  //     // consoleRef.current?.scrollTo({
+  //     //   top: consoleRef.current.scrollHeight,
+  //     //   behavior: "smooth",
+  //     // });
+  //   }, [history]);
 
   return (
     <div className="w-screen h-screen bg-zinc- backdrop-blur-3xl flex flex-col">
       <div
         ref={consoleRef}
-        className="flex-1 p-12 relative overflow-y-auto text-sm font-mono text-green-400"
-        >
-          <span>{`<ENPOINT - URL> `} <b className="text-amber-300">{`<METHOD>`}</b> </span>
+        className="flex-1 p-12 relative overflow-y-auto mask-b-from-70% mask-b-to-100% font-mono text-xs"
+      >
+        <span>
+          {`<ENPOINT - URL> `} <b className="text-amber-300">{`<METHOD>`}</b>{" "}
+          <b className="text-rose-500">{`<BODY>`}</b>{" "}
+        </span>
         {history.map((entry, index) => (
           <div key={index} className="mb-4">
-            <div className="text-green-500 my-2">$ {entry.command}</div>
-            <div className="bg-zinc-950 p-4 shadow-2xl rounded-lg">
-            <JsonNode data={entry.output} INDENT={12} />
-
+            <div className="text-green-500 my-2 flex justify-between">
+              <span className="text-cyan-400">$</span> {entry.command}{" "}
+              <span>{entry.dataResponse?.status}</span>{" "}
             </div>
-            
+            <div className="bg-zinc-950 p-4 shadow-2xl rounded-lg">
+              <JsonNode data={entry.output} INDENT={12} />
+            </div>
           </div>
         ))}
       </div>
 
       <form
         onSubmit={handleSubmit}
-        className="w-full p-4 flex items-center gap-2 border-t border-zinc-800 "
+        className="w-full p-4 flex items-center gap-2 border-t border-zinc-900 "
       >
-        <span className="text-green-400">$</span>
+        <span className="text-green-500"> $ request ~</span>
         <input
           type="text"
           value={consoleText}
           onChange={(e) => setConsoleText(e.target.value)}
           className="flex-1 bg-zinc-900/50 p-2 rounded text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder='Ej: "https://jsonplaceholder.typicode.com/posts POST"'
-          
+          placeholder='Ej: "https://jsonplaceholder.typicode.com/posts <METHOD> <BODY>"'
         />
       </form>
     </div>
