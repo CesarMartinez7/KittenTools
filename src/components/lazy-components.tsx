e0bf0c81d4f432b12b5fb26d67a87734ee341f47
Cesar Martinez <cesarwamartinez@gmail.com>
2025-08-08 17:11:39 -0500

    pass oall to grid

--- src/components/lazy-components.tsx
+++ src/components/lazy-components.tsx
@@ -1,12 +1,12 @@
 import { lazy } from 'react';
 
-export const BaseModalLazy = lazy(() => import('../ui/base-modal/BaseModal'));
-export const GridLayoutLazy = lazy(() => import('../layouts/GridLayout'));
-export const JsonDiffLazy = lazy(() => import('../ui/DiffJson'));
+export const BaseModalLazy = lazy(() => import('../ui/base-modal/BaseModal.tsx'));
+export const GridLayoutLazy = lazy(() => import('../layouts/GridLayout.tsx'));
+export const JsonDiffLazy = lazy(() => import('../ui/DiffJson.tsx'));
 export const JsonViewerLazy = lazy(
   () => import('../ui/formatter-JSON/Formatter'),
 );
-export const ModalViwerJSONLazy = lazy(() => import('../ui/ModalViewer'));
+export const ModalViwerJSONLazy = lazy(() => import('../ui/ModalViewer.tsx'));
 export const CodeEditorLazy = lazy(
   () => import('../ui/code-editor/code-editor'),
 );
