9be0aaeb16b0fe9c8cc0c4e183cde5d5def5f309
Cesar Martinez <cesarwamartinez@gmail.com>
2025-09-03 17:56:58 -0500

    añadiendo logica de creacion de entornos

--- src/pages/client/components/enviroment/enviroment.component.tsx
+++ src/pages/client/components/enviroment/enviroment.component.tsx
@@ -417,6 +417,14 @@ export default function EnviromentComponent() {
             >
               Importar entornos
             </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={toggleImportModalOpen}
+              className="mt-4 px-4 py-2 bg-gray-300 text-shadow-2xs dark:text-zinc-300 text-zinc-800 dark:bg-zinc-800 hover:bg-zinc-700 rounded-md hover:text-white transition-colors"
+            >
+              Crear entondsfdsf
+            </motion.button>
           </motion.div>
         )}
       </AnimatePresence>
